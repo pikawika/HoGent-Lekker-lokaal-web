ffcd39d5e3df8b20fb82a38dfa4cd88ffd0bda95
BramDC3 <bram.deconinck.y7538@student.hogent.be>
2018-02-20 13:43:21 +0100

    Project Aangemaakt

---
+++ LekkerLokaal/LekkerLokaal/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LekkerLokaal.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
