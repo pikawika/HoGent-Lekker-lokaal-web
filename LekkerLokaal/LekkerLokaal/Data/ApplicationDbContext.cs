73d1d6f610adf874a7481493c4b7fa0e2a976d50
Brent <brent_schets@hotmail.be>
2018-02-26 22:21:42 +0100

    Problemen opgelost

--- LekkerLokaal/LekkerLokaal/Data/ApplicationDbContext.cs
+++ LekkerLokaal/LekkerLokaal/Data/ApplicationDbContext.cs
@@ -11,7 +11,11 @@ namespace LekkerLokaal.Data
         public DbSet<Bestelling> Bestelling { get; set; }
         public DbSet<Bon> Bon { get; set; }
         public DbSet<Categorie> Categorie { get; set; }
+<<<<<<< HEAD
         public DbSet<Persoon> Persoon { get; set; }
+=======
+        public DbSet<Klant> Klant { get; set; }
+>>>>>>> da95159384280d3a4363e7317c6f00f9b90b9167
         public DbSet<Handelaar> Handelaar { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
