339a9afed3cf048e81c9fddcd537ef39bdd6dbc8
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-03-19 17:02:43 +0100

    Viewmodel in index
    :smile:

---
+++ LekkerLokaal/LekkerLokaal/Models/HomeViewModels/IndexAlleBonnenLijstModel.cs
@@ -0,0 +1,37 @@
+﻿using LekkerLokaal.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LekkerLokaal.Models.HomeViewModels
+{
+    public class IndexAlleBonnenLijstModel
+    {
+
+        
+        public string GetThumbPath { get; }
+        public string Gemeente { get; }
+        public int AantalBesteld { get; }
+        public string Naam { get; }
+        public string Beschrijving { get; }
+        public decimal MinPrijs { get; }
+        public int BonId { get; }
+
+        public IndexAlleBonnenLijstModel()
+        {
+        }
+
+        public IndexAlleBonnenLijstModel(Bon bon)
+        {
+            Naam = bon.Naam;
+            MinPrijs = bon.MinPrijs;
+            Beschrijving = bon.Beschrijving;
+            AantalBesteld = bon.AantalBesteld;
+            GetThumbPath = bon.GetThumbPath();
+            Gemeente = bon.Gemeente;
+            BonId = bon.BonId;
+        }
+
+    }
+}
---
+++ LekkerLokaal/LekkerLokaal/Models/HomeViewModels/IndexViewModel.cs
@@ -0,0 +1,28 @@
+﻿using LekkerLokaal.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LekkerLokaal.Models.HomeViewModels
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<IndexAlleBonnenLijstModel> AlleBonnen { get; }
+
+        public IEnumerable<IndexTop3BonnenLijstModel> Top3Bonnen { get; }
+
+        public IEnumerable<IndexCategorieMetAantalLijstModel> Top9CategorieMetAantal { get; }
+
+        public IndexViewModel()
+        {
+        }
+        public IndexViewModel(IEnumerable<Bon> alleBonnen, IEnumerable<Bon> top3Bonnen, Dictionary<Categorie, int> top9CategorieMetAantal)
+        {
+            AlleBonnen = alleBonnen.Select(b => new IndexAlleBonnenLijstModel(b)).ToList();
+            Top3Bonnen = top3Bonnen.Select(b => new IndexTop3BonnenLijstModel(b)).ToList();
+            Top9CategorieMetAantal = top9CategorieMetAantal.Select(b => new IndexCategorieMetAantalLijstModel(b)).ToList();
+        }
+
+    }
+}
