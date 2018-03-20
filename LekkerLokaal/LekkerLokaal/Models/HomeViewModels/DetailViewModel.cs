6224885fcaa2954c13cace6828f865179b4bc378
Brent <brent_schets@hotmail.be>
2018-03-20 11:44:06 +0100

    Detail pagina afgewerkt
    Google maps toegevoegd, Naam handelaar en beschrijving van handelaar toegevoegd.
    Samen met Lennert

--- LekkerLokaal/LekkerLokaal/Models/HomeViewModels/DetailViewModel.cs
+++ LekkerLokaal/LekkerLokaal/Models/HomeViewModels/DetailViewModel.cs
@@ -14,6 +14,13 @@ namespace LekkerLokaal.Models.HomeViewModels
         public string Beschrijving { get; }
         public int AantalBesteld { get; }
         public List<string> getAfbeeldingenPathLijst { get; }
+        public string HandelaarNaam { get; }
+        public string HandelaarBeschrijving { get; }
+        public string FormatedAdress { get; }
+        public string Straat { get; }
+        public string Nummer { get; }
+        public string Postcode { get; }
+        public string Gemeente { get; }
 
         public DetailViewModel()
         {
@@ -26,7 +33,9 @@ namespace LekkerLokaal.Models.HomeViewModels
             Beschrijving = bon.Beschrijving;
             AantalBesteld = bon.AantalBesteld;
             getAfbeeldingenPathLijst = bon.getAfbeeldingenPathLijst();
-            
+            HandelaarNaam = bon.Handelaar.Naam;
+            HandelaarBeschrijving = bon.Handelaar.Beschrijving;
+            FormatedAdress = bon.Postcode + " " + bon.Gemeente + ", " + bon.Straat + " " + bon.Huisnummer;
         }
 
         
