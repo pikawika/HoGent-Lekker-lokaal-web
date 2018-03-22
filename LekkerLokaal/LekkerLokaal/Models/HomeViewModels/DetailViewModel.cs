69465c378847bd53ffc61c342050f4c769864981
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-03-22 15:15:57 +0100

    Detail pagina add to card

--- LekkerLokaal/LekkerLokaal/Models/HomeViewModels/DetailViewModel.cs
+++ LekkerLokaal/LekkerLokaal/Models/HomeViewModels/DetailViewModel.cs
@@ -13,16 +13,16 @@ namespace LekkerLokaal.Models.HomeViewModels
         public int BonId { get; }
         public string Naam { get; }
         public decimal MinPrijs { get; }
+        public decimal MaxPrijs { get; }
         public string Beschrijving { get; }
         public int AantalBesteld { get; }
         public List<string> getAfbeeldingenPathLijst { get; }
         public string HandelaarNaam { get; }
         public string HandelaarBeschrijving { get; }
         public string FormatedAdress { get; }
-        public string Straat { get; }
-        public string Nummer { get; }
-        public string Postcode { get; }
         public string Gemeente { get; }
+        public string CategorieIcon { get; }
+        public string CategorieNaam { get; }
 
         public DetailViewModel()
         {
@@ -33,12 +33,16 @@ namespace LekkerLokaal.Models.HomeViewModels
             BonId = bon.BonId;
             Naam = bon.Naam;
             MinPrijs = bon.MinPrijs;
+            MaxPrijs = bon.MaxPrijs;
             Beschrijving = bon.Beschrijving;
             AantalBesteld = bon.AantalBesteld;
             getAfbeeldingenPathLijst = bon.getAfbeeldingenPathLijst();
             HandelaarNaam = bon.Handelaar.Naam;
             HandelaarBeschrijving = bon.Handelaar.Beschrijving;
             FormatedAdress = bon.Postcode + " " + bon.Gemeente + ", " + bon.Straat + " " + bon.Huisnummer;
+            Gemeente = bon.Gemeente;
+            CategorieIcon = bon.Categorie.Icon;
+            CategorieNaam = bon.Categorie.Naam;
         }
     }
 }
