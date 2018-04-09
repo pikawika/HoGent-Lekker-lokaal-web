a75aa968bfac7258e516f63f29059636e9aa878b
BramDC3 <bram.deconinck.y7538@student.hogent.be>
2018-04-09 22:00:04 +0200

    Text fixes

--- LekkerLokaal/LekkerLokaal/Extensions/EmailSenderExtensions.cs
+++ LekkerLokaal/LekkerLokaal/Extensions/EmailSenderExtensions.cs
@@ -15,7 +15,7 @@ namespace LekkerLokaal.Services
                 $"<p>Hallo {voornaam}!</p>" +
                 $"<p>Bedankt voor uw registratie bij Lekker Lokaal.</p>" +
                 $"<p>Gelieve <a href='{HtmlEncoder.Default.Encode(link)}'>hier te klikken</a> om uw e-mailadres te bevestigen zodat uw account aangemaakt kan worden.</p>" +
-                $"<p>Lukt dit niet? Druk dan op deze link: {HtmlEncoder.Default.Encode(link)} </p>" + 
+                $"<p>Lukt dit niet? Druk dan op deze link: <a href='{HtmlEncoder.Default.Encode(link)}'>{HtmlEncoder.Default.Encode(link)}</a></p>" + 
                 $"<p>Indien u niet degene bent die een account wou aanmaken bij Lekker Lokaal, mag u deze e-mail negeren. </p>" +
                 $"<p>Met vriendelijke groeten, </p>" +
                 $"<p>Het Lekker Lokaal team </p>");
