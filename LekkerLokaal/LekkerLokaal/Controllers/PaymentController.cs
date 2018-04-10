82cef83fb9a10b096a85a50ddba5291f7e892d38
Brent <brent_schets@hotmail.be>
2018-04-11 00:26:03 +0200

    uitwerken van PaymenyController en request

--- LekkerLokaal/LekkerLokaal/Controllers/PaymentController.cs
+++ LekkerLokaal/LekkerLokaal/Controllers/PaymentController.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Mollie.Api.Client;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Models.Payment.Request;
+using Mollie.Api.Models.Payment.Response;
 
 namespace LekkerLokaal.Controllers
 {
     public class PaymentController : Controller
     {
-        public IActionResult Index()
+        private readonly IPaymentClient _paymentClient;
+
+        public PaymentController()
         {
-            return View();
+            this._paymentClient = new PaymentClient("test_yGJ4USbh3BWV5AkGbdh4NG4EG2UdaF");
         }
+
+        [HttpGet]
+        public async Task<ActionResult> Pay(string id)
+        {
+            PaymentResponse payment = await this._paymentClient.GetPaymentAsync(id);
+            return this.Redirect(payment.Links.PaymentUrl);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            PaymentRequestModel payment = new PaymentRequestModel();
+            return this.View(payment);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(PaymentRequestModel paymentRequestModel)
+        {
+            if (this.ModelState.IsValid)
+            {
+                PaymentRequest paymentRequest = new PaymentRequest();
+                paymentRequest.Amount = paymentRequestModel.Amount;
+                paymentRequest.Description = paymentRequestModel.Description;
+                paymentRequest.RedirectUrl = paymentRequestModel.RedirectUrl;
+                await this._paymentClient.CreatePaymentAsync(paymentRequest);
+
+                return this.RedirectToAction("Index");
+            }
+
+            return this.View(paymentRequestModel);
+        }
+
     }
 }
--- LekkerLokaal/LekkerLokaal/Models/PaymentViewModels/PaymentRequestModel.cs
+++ LekkerLokaal/LekkerLokaal/Models/PaymentViewModels/PaymentRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace LekkerLokaal.Models.PaymentViewModels
 {
     public class PaymentRequestModel
     {
+        [Required]
+        [Range(1, 1000)]
+        public decimal Amount { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Url]
+        public string RedirectUrl { get; set; }
     }
 }
