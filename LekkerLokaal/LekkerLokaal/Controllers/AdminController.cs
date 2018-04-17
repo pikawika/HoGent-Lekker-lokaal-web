a2cc524130b116cd1fe3edc3cb77a274a38eb123
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-04-17 15:03:41 +0200

    Admin panel
    Begin admin panel hard coded

---
+++ LekkerLokaal/LekkerLokaal/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LekkerLokaal.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult VerkochteCadeaubonnen()
+        {
+            return View();
+        }
+
+        public IActionResult UitbetaaldeCadeaubonnen()
+        {
+            return View();
+        }
+    }
+}
