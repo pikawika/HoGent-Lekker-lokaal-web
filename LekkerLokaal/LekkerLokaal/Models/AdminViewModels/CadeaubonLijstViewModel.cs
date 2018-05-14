9e33bb2be669a86fb7e1fa6156ab18e101d7d9d6
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-05-14 17:44:40 +0200

    cadeaubonoverzicht

---
+++ LekkerLokaal/LekkerLokaal/Models/AdminViewModels/CadeaubonLijstViewModel.cs
@@ -0,0 +1,27 @@
+﻿using LekkerLokaal.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LekkerLokaal.Models.AdminViewModels
+{
+    public class CadeaubonLijstViewModel
+    {
+        public int Id { get; }
+        public string Gemeente { get; }
+        public string Naam { get; }
+
+        public int AantalBonnenInSysteem { get; }
+
+        public CadeaubonLijstViewModel(Bon bon)
+        {
+            Id = bon.BonId;
+            Gemeente = bon.Gemeente;
+            Naam = bon.Naam;
+            AantalBonnenInSysteem = bon.AantalBesteld;
+        }
+
+
+    }
+}
