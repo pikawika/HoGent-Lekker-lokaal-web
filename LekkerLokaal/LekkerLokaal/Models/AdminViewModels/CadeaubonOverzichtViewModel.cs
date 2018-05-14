b5943c243c896a2fa78b1b356bb6882614b7d329
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-05-14 17:44:40 +0200

    cadeaubonoverzicht

---
+++ LekkerLokaal/LekkerLokaal/Models/AdminViewModels/CadeaubonOverzichtViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LekkerLokaal.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace LekkerLokaal.Models.AdminViewModels
+{
+    public class CadeaubonOverzichtViewModel
+    {
+        public IEnumerable<CadeaubonLijstViewModel> AlleCadeaubonnenGesorteerdOpId { get; }
+
+        public CadeaubonOverzichtViewModel(IEnumerable<Bon> alleCadeaubonnen)
+        {
+            AlleCadeaubonnenGesorteerdOpId = alleCadeaubonnen.Select(c => new CadeaubonLijstViewModel(c)).ToList();
+        }
+
+    }
+}
