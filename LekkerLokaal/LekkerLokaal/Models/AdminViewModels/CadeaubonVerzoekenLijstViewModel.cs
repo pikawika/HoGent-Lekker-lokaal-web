eccbf411cf7fe65356797b2bbf22d8b7ced51764
Lennert <lennert.bontinck.y9785@student.hogent.be>
2018-05-16 23:03:28 +0200

    bonverzoek overzicht

--- LekkerLokaal/LekkerLokaal/Models/AdminViewModels/CadeaubonVerzoekenLijstViewModel.cs
+++ LekkerLokaal/LekkerLokaal/Models/AdminViewModels/CadeaubonVerzoekenLijstViewModel.cs
@@ -1,29 +1,25 @@
-﻿using LekkerLokaal.Models.Domain;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LekkerLokaal.Models.Domain;
 
 namespace LekkerLokaal.Models.AdminViewModels
 {
-    public class CadeaubonLijstViewModel
+    public class CadeaubonVerzoekenLijstViewModel
     {
         public string Handelaarnaam { get; }
         public int Id { get; }
         public string Gemeente { get; }
         public string BonNaam { get; }
 
-        public int AantalBonnenInSysteem { get; }
-
-        public CadeaubonLijstViewModel(Bon bon)
+        public CadeaubonVerzoekenLijstViewModel(Bon bon)
         {
             Handelaarnaam = bon.Handelaar.Naam;
             BonNaam = bon.Naam;
             Gemeente = bon.Gemeente;
             Id = bon.BonId;
-            AantalBonnenInSysteem = bon.AantalBesteld;
         }
-
 
     }
 }
