022e864ac70692666d0be7c70168c7165ff0f6c3
BramDC3 <bram.deconinck.y7538@student.hogent.be>
2018-05-21 15:40:01 +0200

    Kleine extra voor rest api

--- LekkerLokaal/LekkerLokaal/Controllers/MobieleAppController.cs
+++ LekkerLokaal/LekkerLokaal/Controllers/MobieleAppController.cs
@@ -77,7 +77,8 @@ namespace LekkerLokaal.Controllers
                         bestellijn.AanmaakDatum,
                         handelaar.HandelaarId,
                         handelaar.Emailadres,
-                        bestellijn.Geldigheid
+                        bestellijn.Geldigheid,
+                        bon.BonId
                     };
                 }
             }
